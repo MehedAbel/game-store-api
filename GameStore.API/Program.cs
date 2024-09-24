c18087dfbecac00d950798e77433c8eac0e0f071
Abel <111386807+abel325@users.noreply.github.com>
2024-09-24 13:18:48 +0300

    add entities and game store context

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,7 +1,12 @@
+using GameStore.API.Data;
 using GameStore.API.DTOs;
 using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source = GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
--- GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807e8e9bf486998ad9ce9704f197f9e7f3f9cb0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3e0298b8f7e752150f346e8c178928073ecb52")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
